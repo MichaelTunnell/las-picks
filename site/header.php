12d4cc076560e426e4bd63d0294f8ebc949d3350
MichaelTunnell <MichaelTunnell@users.noreply.github.com>
2014-12-20 11:44:32 -0600

    GitHub Fork banner added and CSS banner made for smartphones.

--- site/header.php
+++ site/header.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+	<a class="forkongithub" href="https://github.com/MichaelTunnell/las-picks"><span>Fork me on GitHub</span><img src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 	<header>
 		<nav>
 			<ul>
@@ -16,5 +17,5 @@
 		<div class="clr"></div>
 			</ul>
 		</nav>
-		<img src="jb-las-logo-192.jpg" /><br /><h1>Linux Action Show<br /><span><?php echo $pagetitle; ?></span></h1>
+		<img class="logo" src="jb-las-logo-192.jpg" /><br /><h1>Linux Action Show<br /><span><?php echo $pagetitle; ?></span></h1>
 	</header>
