e370e1b0db666729edaa768dcc417710ab117846
TexTheAdmin <textheadmin@users.noreply.github.com>
2014-12-21 23:07:07 -0600

    Updated links LAS 311

--- site/spotlights.php
+++ site/spotlights.php
@@ -171,7 +171,7 @@ include 'header.php';
 			<td><?php echo date("Y-m-d", strtotime($sd2014."+18 weeks")); ?></td>
 		</tr>
 		<tr>
-			<td><a href=""></a></td>
+			<td><a href="http://sourceforge.net/projects/mooos/">mooOS</a></td>
 			<td><a href="<?php echo $las_311_url; ?>"><?php echo $las_311_title; ?></a></td>
 			<td><?php echo date("Y-m-d", strtotime($sd2014."+17 weeks")); ?></td>
 		</tr>
